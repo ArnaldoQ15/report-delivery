fd5059f3c4d7ab5a68ded1e9fd27104291570999
Arnaldo Fagundes <arnaldo.s.fagundes@gmail.com>
2023-08-09 20:14:59 -0300

    Adjustments on architecture files

--- src/main/java/com/report/delivery/util/QRCodeUtils.java
+++ src/main/java/com/report/delivery/util/QRCodeUtils.java
@@ -4,7 +4,6 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.report.delivery.config.exception.BadRequestException;
 import jakarta.annotation.Nullable;
 import lombok.NoArgsConstructor;
@@ -15,6 +14,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.google.zxing.BarcodeFormat.QR_CODE;
+import static com.google.zxing.EncodeHintType.ERROR_CORRECTION;
+import static com.google.zxing.EncodeHintType.MARGIN;
+import static com.google.zxing.qrcode.decoder.ErrorCorrectionLevel.L;
 import static java.util.Objects.isNull;
 import static lombok.AccessLevel.PRIVATE;
 
@@ -22,12 +24,12 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor(access=PRIVATE)
 public class QRCodeUtils {
 
-    public static byte[] gerarQRCode(final String data, final String extension,
-                                     @Nullable final Integer size) throws BadRequestException {
+    public static byte[] generateQrCode(final String data, final String extension,
+                                        @Nullable final Integer size) throws BadRequestException {
         final String charset = "UTF-8";
         final Map<EncodeHintType, Object> hashMap = new HashMap<>();
-        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-        hashMap.put(EncodeHintType.MARGIN, 1);
+        hashMap.put(ERROR_CORRECTION, L);
+        hashMap.put(MARGIN, 1);
 
         try {
             final BitMatrix matrix =
@@ -47,8 +49,8 @@ public class QRCodeUtils {
         return isNull(size) ? 400 : size;
     }
 
-    public static byte[] gerarQRCode(final String destino, final String extension) throws BadRequestException {
-        return gerarQRCode(destino, extension, null);
+    public static byte[] generateQrCode(final String data, final String extension) throws BadRequestException {
+        return generateQrCode(data, extension, null);
     }
 
 }
