2b233d9fd9071ceec445fdc05ce4b43033472c4c
Arnaldo Fagundes <arnaldo.s.fagundes@gmail.com>
2023-08-10 00:36:22 -0300

    Minor architecture adjustment

--- src/main/java/com/report/delivery/config/report/ReportProvider.java
+++ src/main/java/com/report/delivery/config/report/ReportProvider.java
@@ -14,11 +14,14 @@ import java.io.File;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
 import static java.time.LocalDateTime.now;
 import static java.time.format.DateTimeFormatter.ofPattern;
+import static java.util.Locale.setDefault;
+import static java.util.Objects.*;
 import static javax.imageio.ImageIO.read;
 import static net.sf.jasperreports.engine.JasperExportManager.exportReportToPdf;
 import static net.sf.jasperreports.engine.JasperFillManager.fillReport;
@@ -30,10 +33,17 @@ public class ReportProvider {
     private static final String ERROR_REPORT = "error.report";
     private static final String PATTERN_FILENAME = "ddMMyyyy_HHmmss";
 
+    static {
+        final Locale locale = new Locale("en", "US");
+        setDefault(locale);
+    }
+
     public ReportData getReportData(final String reportName, final Map<String, Object> parameters,
                                     final ReportType format, final List<Object> items) throws BadRequestException {
         try {
 
+            parameters.put("REPORT_BASEDIR", getBaseDir());
+            parameters.put("REPORT_LOGO_DIR", getBaseDir() + "resources/logo.png");
             final LocalDateTime dateTime = now();
             final String filename =
                     reportName.split("\\.")[0] + "_" + dateTime.format(ofPattern(PATTERN_FILENAME)) +
@@ -53,11 +63,21 @@ public class ReportProvider {
     private File getFile(final String reportName) throws BadRequestException {
         final URL resource = getClass().getResource("/report/" + reportName + ".jasper");
 
-        if (Objects.isNull(resource)) {
+        if (isNull(resource)) {
             throw new BadRequestException(ERROR_REPORT);
         }
 
         return new File(resource.getFile());
+    }
+
+    private String getBaseDir() throws BadRequestException {
+        final URL resource = getClass().getResource("/report/");
+
+        if (isNull(resource)) {
+            throw new BadRequestException(ERROR_REPORT);
+        }
+
+        return resource.getPath();
     }
 
     public Object getImageFromByteArray(final byte[] data) throws BadRequestException {
